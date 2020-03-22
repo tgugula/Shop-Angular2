df32ee56582965f6068470461bb798ed3f75658a
tgugula <tgugula@wszib.edu.pl>
2020-03-22 23:13:08 +0100

    dodano shipping

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {ShopComponent} from "./shop/shop.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'shop', component: ShopComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ShopComponent } from './shop/shop.component';
+import { ShippingComponent } from './shop/shipping/shipping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ShopComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule,
